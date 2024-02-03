f9e65140d9f97869a133897b63d4ac8f882db69c
bibhu-otz <bib.mhnt@gmail.com>
2024-02-03 19:38:49 +0530

    array

---
+++ 03-02-2024/testArray.js
@@ -0,0 +1,56 @@
+//Dense Array
+
+// let arr = ["A", "B", "C"];
+// console.log(arr.length);
+
+// arr[50] = 80;
+
+// console.log(arr.length);
+// console.log(arr[3]);
+
+// let arr1 = new Array(40);
+// console.log("Length of arr1 is " + arr1.length);
+// console.log(arr1[0]);
+
+// let arr3 = [10, 20, 30];
+// console.log("Item at index 1 is " + arr3[1]);
+// delete arr3[1];
+// console.log("Item at index 1 is " + arr3[1]);
+
+// var arr4 = [1, 2, 3, 4];
+// var arr5 = arr4;
+// console.log(arr4, arr5);
+// arr5[0] = 70;
+// console.log(arr4, arr5);
+
+// var arr4 = [1, 2, 3, 4];
+// var arr5 = Array.from(arr4);
+// console.log(arr4, arr5);
+// arr5[0] = 70;
+// console.log(arr4, arr5);
+
+// let arr = [1, 2, 3, 4];
+
+// console.log(arr);
+
+// let arr1 = arr.slice(1, 3);
+
+// console.log(arr1);
+
+// let name = ["Alok Kumar", "Arbind Padhi", "Rabi Sankar"];
+// let lastname = name.slice(0, -2);
+// console.log(lastname);
+
+// let arr = [1, 2, 3, 4];
+// let add = arr.splice(0, 2, 10);
+// console.log(arr);
+// console.log(add);
+
+function sum(x, y) {
+  return x + y;
+}
+
+function add(x, y) {
+  return sum(x, y);
+}
+console.log(add(10, 20));
